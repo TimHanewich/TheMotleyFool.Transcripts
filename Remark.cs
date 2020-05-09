604eecf0e9525b721d3ff46f14838d49d9bb7b44
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-05-08 20:37:40 -0700

    Completed base functionality

---
+++ Remark.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace TheMotleyFool.Transcripts
+{
+    public class Remark
+    {
+        public CallParticipant Speaker {get; set;}
+        public string[] SpokenRemarks {get; set;}
+    }
+}
